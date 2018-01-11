f0fdc73926d661b338cb2ff9698ce64342d1a527
Thibaut <thibaut.piquerez@he-arc.ch>
2018-01-11 09:35:01 +0100

    ajout de la première version de l'IA

---
+++ SolutionOthelloHeroesBattle/OthelloHeroesBattle/AI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using IPlayable;
+
+namespace OthelloHeroesBattle
+{ 
+    class AI
+    {
+        private const int MAXDEPTH = 4;
+
+        private IPlayable.IPlayable board;
+        private int color;
+        private Tuple<int, int> bestMove;
+        
+        public AI(IPlayable.IPlayable board, int color)
+        {
+            this.board = board;
+            this.color = color;
+        }
+
+        public Tuple<int,int> GetNextMove()
+        {
+            GameState currentState = new GameState(board.GetBoard(), color);
+            AlphaBeta(currentState, MAXDEPTH, 1, currentState.GetEvaluation());
+            return bestMove;
+        }
+ 
+        private int AlphaBeta(GameState gameState, int depth, int minOrMax, int parentValue)
+        {
+            if(depth == 0 || gameState.IsFinal())
+            {
+                return gameState.GetEvaluation();
+            }
+            int bestEvaluation = minOrMax * -int.MaxValue;
+            bestMove = null;
+            foreach (Tuple<int,int> move in gameState.GetAvaibleMove())
+            {
+                GameState newState = gameState.ApllyMove(move);
+                int tempEvaluation = AlphaBeta(newState, depth - 1, -minOrMax, bestEvaluation);
+                if(tempEvaluation * minOrMax > bestEvaluation * minOrMax)
+                {
+                    bestEvaluation = tempEvaluation;
+                    bestMove = move;
+                    if(bestEvaluation * minOrMax > parentValue * minOrMax)
+                    {
+                        break;
+                    }
+                }
+            }
+            return bestEvaluation;
+        }
+
+
+        
+
+
+    }
+}
---
+++ SolutionOthelloHeroesBattle/OthelloHeroesBattle/GameState.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloHeroesBattle
+{
+    class GameState
+    {
+        private const int WEIGHTBORDER = 4;
+        private const int WEIGHTCENTER = 1;
+
+        private int color;
+        private int[,] state;
+        private int weight;
+
+        public GameState(int[,] state, int color)
+        {
+            this.color = color;
+            this.state = state;
+            this.weight = GetEvaluation();          
+        }
+    
+        public bool IsFinal()
+        {
+            foreach (int value in state)
+            {
+                if(value != color && value != -1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public GameState ApllyMove(Tuple<int,int> move)
+        {
+            int[,] newState = state;
+            newState[move.Item1, move.Item2] = color;
+            int newColor = color - 1;
+            if (newColor < 0) newColor = 1;
+            return new GameState(newState, newColor);
+        }
+
+        public List<Tuple<int, int>> GetAvaibleMove()
+        {
+            //TODO
+            return new List<Tuple<int, int>>();
+        }
+
+        public int GetEvaluation()
+        {
+            int tempWeight = GetWeightColor();
+
+            //TODO fonction renvoyant le nombre de coup possible de chaque couleur
+
+            return tempWeight;
+        }
+
+        private int GetWeightColor()
+        {         
+            int counter = 0;
+            int sizeSide = (int)Math.Sqrt(state.Length);
+
+            for (int i = 0; i < sizeSide; i++)
+            {
+                for (int j = 0; j < sizeSide; j++)
+                {
+                    int value = state[i, j];
+                    if (value == 0)//white
+                    {
+                        value = -1;
+                    }
+
+                    if (i % (sizeSide - 1) == 0 || j % (sizeSide - 1) == 0)
+                    {
+                        counter += WEIGHTBORDER * value;
+                    }
+                    else
+                    {
+                        counter += WEIGHTCENTER * value;
+                    }
+                }
+            }
+            return counter;
+        }
+
+        
+    }
+}
